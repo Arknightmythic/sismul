1716865917268d9244db4a6585ebd5d801578e75
Arknightmythic <renokristiardy@gmail.com>
2024-04-20 12:54:19 +0700

    update

--- application/models/M_welcome.php
+++ application/models/M_welcome.php
@@ -2,6 +2,16 @@
 defined("BASEPATH") OR exit('No direct script access allowed');
 
 class M_welcome extends CI_Model {
+    public function read($id = FALSE){
+        if ($id === FALSE){
+            return $this->db->get('post')->result_array();
+        }
+        else{
+            $query = $this->db->get_where('post',array('id' => $id));
+            return $query->row();
+        }
+    }
+
     public function create($id, $filename){
         $data = [
             'id' => $id,
---
+++ application/views/home.php
@@ -0,0 +1,17 @@
+<?php
+echo $this->session->flashdata('error');
+?>
+
+<ul class="collection">
+    <?php foreach ($home_post as $data):?>
+        <li class="collection-item avatar">
+            <img src="<?=site_url("/upload/post/".$data['filename']);?>" class="circle">
+            <p class="title""><?= $data['name']; ?></p>
+            <small><?= $data['description']?></small>
+            <a href="<?= site_url("welcome/index/".$data['id']); ?>"class="secondary content">
+                <i class ="material-icons" style= "margin-left: 100px">visibility</i>
+            </a>
+        </li>
+    <?php endforeach; ?>
+
+</ul>
